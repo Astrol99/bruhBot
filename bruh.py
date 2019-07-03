b757886a1fb3a87b07b7a7e3fee3a029cfff1a03
Dav2006 <dngames2006@gmail.com>
2019-07-03 15:15:12 -0500

    Made functionality to load data from json file

--- bruh.py
+++ bruh.py
@@ -1,2 +1,9 @@
 import discord
 from discord.ext import commands
+import json
+
+# Extracts token and prefix from resources.json
+with open("resources.json") as resources:
+    data = json.load(resources)
+    TOKEN = data['token']
+    PREFIX = data['prefix']
