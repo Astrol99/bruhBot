d89906e592b03d9c5fd951587da6f75fe422ac41
Dav2006 <dngames2006@gmail.com>
2019-07-03 15:30:30 -0500

    Created bot foundation

--- bruh.py
+++ bruh.py
@@ -7,3 +7,15 @@ with open("resources.json") as resources:
     data = json.load(resources)
     TOKEN = data['token']
     PREFIX = data['prefix']
+
+# Create bot object
+bot = commands.Bot(command_prefix=PREFIX)
+
+@bot.event # Runs this function when bot is started
+async def on_ready():
+    # Info from bot
+    print("Bot online!")
+    print(f"\nTOKEN: {TOKEN}")
+    print(f"PREFIX: {PREFIX}")
+
+bot.run(TOKEN)
